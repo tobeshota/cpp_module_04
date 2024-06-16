451aa1f4475d52c20617376b82762b74d9ac78b1
toshota <toshota@student.42tokyo.jp>
2024-06-16 21:02:42 +0900

    [doc] ex00/

--- ex00/Animal.hpp
+++ ex00/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: toshota <toshota@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 21:56:37 by tobeshota         #+#    #+#             */
-/*   Updated: 2024/06/16 20:06:33 by toshota          ###   ########.fr       */
+/*   Updated: 2024/06/16 21:02:28 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,15 @@ class Animal {
   Animal& operator=(const Animal& other);
   virtual ~Animal(void);
   std::string getType(void) const;
+  /** 仮想関数 c.f．https://cpp-lang.sevendays-study.com/ex-day6.html
+   * virtualがついたメソッドを関数と呼ぶ，
+   * メソッドにvirtualをつけると，もし子クラスにオーバーライドされたものがある場合，子クラスに実装されたメンバ関数が実行される．
+   * 仮想関数でないメンバ関数がオーバーライドされた場合，親クラスから子クラスのそれを呼び出すことはできない．
+   * しかし，virtualをつけ仮想化することで，親クラスから子クラスに実装・オーバーロードされたメンバ関数を呼び出すことができる．つまり，下記に示すことができる．
+   *  CParent human = new CChild();  //  親クラスの変数を子クラスで動的に初期化する．
+   *  human.overridedfunc();         //  親子ともに定義されたメソッドを用いる．このとき，overridedfunc()が仮想関数の場合，子クラスのoverridedfunc()が呼ばれ，仮想関数でない場合，親クラスのそれが呼ばれる．
+   */
+  // virtualがついたメソッドは，サブクラスに実装された，メンバ関数が実行されています。
   virtual void makeSound(void) const; //  仮想関数
 };
 
