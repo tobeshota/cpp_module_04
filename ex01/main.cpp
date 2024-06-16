995664f0a23ee045afe93908df95bbbb1e7bdafd
toshota <toshota@student.42tokyo.jp>
2024-06-16 13:45:55 +0900

    [add] add test in ex01/main.cpp

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,44 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tobeshota <tobeshota@student.42.fr>        +#+  +:+       +#+        */
+/*   By: toshota <toshota@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 22:18:45 by tobeshota         #+#    #+#             */
-/*   Updated: 2024/06/16 11:13:03 by tobeshota        ###   ########.fr       */
+/*   Updated: 2024/06/16 13:45:18 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
 #include "Dog.hpp"
 
-// static void put_line(void)
+static void put_line(void)
+{
+  static int index;
+  std::cout << "- " << index++ << " ────────────────────" << std::endl;
+}
+
+// static void put_miniline(void)
 // {
-//   static int index;
-//   std::cout << "- " << index++ << " ────────────────────" << std::endl;
+//   std::cout << "─────" << std::endl;
 // }
 
 
+// int main()
+// {
+//   Animal* animals[10];
+//   for (int i = 0; i < 5; ++i) {
+//     animals[i] = new Dog();
+//     put_miniline();
+//     animals[i + 5] = new Cat();
+//     put_miniline();
+//   }
+
+//   put_line();
+
+//   for (int i = 0; i < 10; ++i) {
+//     animals[i]->makeSound();
+//     put_miniline();
+//     delete animals[i];
+//     put_miniline();
+//   }
+// }
+
 int main() {
-  const Animal* j = new Dog();
-  const Animal* i = new Cat();
+  const Animal* j = new Dog();  //  ①(constructor)Animal Default constructor called ②(constructor)Brain Default constructor called ③(constructor)Dog Default constructor called
 
-  j->makeSound();
-  i->makeSound();
+  put_line();
+  const Animal* i = new Cat();  //  ④(constructor)Animal Default constructor called ⑤(constructor)Brain Default constructor called ⑥(constructor)Cat Default constructor called
+  put_line();
+  j->makeSound();               //  ⑦(Dog sound)Ahwoooooo
+  put_line();
+  i->makeSound();               //  ⑧(Cat sound)meowwwww
+  put_line();
 
-  delete j; //  should not create a leak
-  delete i;
-
-  // Animal array;
-  Animal* animals[10];
-  for (int k = 0; k < 5; ++k) {
-    animals[k] = new Dog();
-    animals[k + 5] = new Cat();
-  }
-
-  for (int k = 0; k < 10; ++k) {
-    animals[k]->makeSound();
-    delete animals[k];
-  }
+  //  should not create a leak
+  delete j;                     //  ⑨(constructor)Brain destructor called ⑩(constructor)Dog destructor called ⑪(constructor)Animal destructor called
+  put_line();
+  delete i;                     //  ⑫(constructor)Brain destructor called ⑬(constructor)Cat destructor called ⑭(constructor)Animal destructor called
 
   return 0;
 }
